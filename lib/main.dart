11036004005f7f7781ca11db11e5327541fc00e4
SarthakChandervanshi <54674615+SarthakChandervanshi@users.noreply.github.com>
2021-08-01 09:15:25 +0530

    Made changes to title of app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Internship Assigment",
+      title: "Internship Assignment",
       theme: ThemeData.light(),
       debugShowCheckedModeBanner: false,
       initialRoute: 'username',
